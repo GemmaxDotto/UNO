3a3c61d8371fc5e9460e99ffb3eb7f000c0f6437
GemmaxDotto <g.dotto05@gmail.com>
2023-11-11 17:52:05 +0100

    ClientTCP_commit3

--- uno_nobuild/src/client/App.java
+++ uno_nobuild/src/client/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-         TCPClient client=new TCPClient("localhost",666);
+         TCPClient client=new TCPClient("localhost",12346);
          client.sendMessage("nome;start");
+        client.receiveMessage();
     }
 }
