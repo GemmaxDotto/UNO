c5271ab7d058bd882d68b26e138d945023fcdf15
GemmaxDotto <g.dotto05@gmail.com>
2023-11-11 16:38:27 +0100

    ServerTCP giusto

--- uno_nobuild/src/server/server.py
+++ uno_nobuild/src/server/server.py
@@ -1,27 +1,51 @@
 import socket
-import sys
+import threading
 
-def invia_comandi(s):
-    while True:
-        comando = input("-> ")
-        if comando == "ESC":
-            print("Sto chiudendo la connessione col Server.")
-            s.close()
-            sys.exit()
-        else:
-            s.send(comando.encode())
-            data = s.recv(4096)
-            print(str(data, "utf-8"))
-
-def conn_sub_server(indirizzo_server):
+def handle_client(client_socket):
     try:
-        s = socket.socket()             # creazione socket client
-        s.connect(indirizzo_server)     # connessione al server
-        print(f"Connessessione al Server: { indirizzo_server } effettuata.")
-    except socket.error as errore:
-        print(f"Qualcosa è andato storto, sto uscendo... \n{errore}")
-        sys.exit()
-    invia_comandi(s)
+        # Ricevi i dati dal client
+        data = client_socket.recv(1024)
+        print(f"Ricevuto: {data.decode()}")
 
-if __name__ == '__main__':
-    conn_sub_server(("192.168.1.6", 20000))
+        # Puoi qui gestire la logica del gioco Uno in base ai dati ricevuti
+        # Ad esempio, aggiornare lo stato di gioco, rispondere con un messaggio, ecc.
+
+    except Exception as e:
+        print(f"Errore durante la gestione del client: {e}")
+    finally:
+        # Chiudi la connessione
+        client_socket.close()
+
+def start_server():
+    # Specifica l'indirizzo IP e la porta su cui il server ascolterà le connessioni
+    server_address = "localhost"
+    server_port = 666
+
+    # Crea il socket del server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Associa il socket all'indirizzo IP e alla porta
+    server_socket.bind((server_address, server_port))
+
+    # Metti il server in ascolto su un massimo di 5 connessioni in coda
+    server_socket.listen(5)
+    print(f"Server in ascolto su {server_address}:{server_port}")
+
+    try:
+        while True:
+            # Accetta la connessione dal client
+            client_socket, client_address = server_socket.accept()
+            print(f"Connessione accettata da {client_address}")
+
+            # Crea un thread per gestire il client
+            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+            client_thread.start()
+
+    except KeyboardInterrupt:
+        print("Server terminato manualmente.")
+    finally:
+        # Chiudi il socket del server
+        server_socket.close()
+
+if __name__ == "__main__":
+    start_server()
