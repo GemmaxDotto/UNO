6658af407c5afb818a5bb174a6f7c7d32be52ab5
GemmaxDotto <g.dotto05@gmail.com>
2023-11-22 20:09:29 +0100

    aggiunta righe per sistemare la immagine nel componente,implementazione quando viene cliccata una carta del giocatore

--- uno_nobuild/src/client/CardComponent.java
+++ uno_nobuild/src/client/CardComponent.java
@@ -1,27 +1,40 @@
-import javax.swing.*;
 
 import org.w3c.dom.events.MouseEvent;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class CardComponent extends JButton {
     private UnoCard card;
     GameManaging Game;
+    ClientGUI GUI;
 
-    public CardComponent(UnoCard card,GameManaging Game) {
+    public CardComponent(UnoCard card, GameManaging Game,ClientGUI GUI) {
         this.card = card;
-       // setText(card.toString());  // Usa toString() o un'altra rappresentazione testuale della carta
-        //setOpaque(true);
-        //setBackground(Color.WHITE);
-        setIcon(new ImageIcon(card.getImagePath()));  // Assicurati che l'immagine venga impostata correttamente
-        setPreferredSize(new Dimension(50, 80));  // Imposta le dimensioni desiderate
-        setFocusable(true);  // Abilita l'evento di input
+        // setText(card.toString()); // Usa toString() o un'altra rappresentazione
+        // testuale della carta
+        // setOpaque(true);
+        // setBackground(Color.WHITE);
+        this.GUI=GUI;
+        ImageIcon originalIcon = new ImageIcon(card.getImagePath());
+
+        // Ridimensiona l'immagine
+        Image originalImage = originalIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(50, 80, Image.SCALE_SMOOTH); // Imposta le dimensioni
+                                                                                         // desiderate
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        setIcon(scaledIcon);
+        setPreferredSize(new Dimension(50, 80)); // Imposta le dimensioni desiderate
+        setFocusable(true); // Abilita l'evento di input
         addMouseListener(new CardClickListener());
+        // setBorder(BorderFactory.createLineBorder(Color.RED)); // Cambia il colore a
+        // tuo piacimento
+        // Oppure
+        // setBackground(Color.YELLOW); // Cambia il colore a tuo piacimento */
     }
 
     public UnoCard getCard() {
@@ -31,12 +44,23 @@ public class CardComponent extends JButton {
     private class CardClickListener extends MouseAdapter {
         @Override
         public void mouseClicked(java.awt.event.MouseEvent e) {
-            // Chiamato quando la carta viene cliccata
-            System.out.println("Hai cliccato su: " + card);
-            // Puoi fare qualcos'altro con la carta, ad esempio notificarla al tuo GameManaging
-            Game.onCardClicked(card);
+            // Ottieni la carta associata a questo componente
+            UnoCard clickedCard = getCard();
+
+            // Invia un messaggio al server
+            // Assicurati che il tuo oggetto GameManaging abbia un metodo
+            // sendMessageToServer()
+            // che possa inviare il messaggio al server
+            //&&clickedCard.toString().(Game.myCards)
+            if (Game != null&&!clickedCard.getColore().equals("K")) {
+                Game.client.sendMessage(Game.nickNameString+";"+"lascia;"+clickedCard.toString());
+                Game.setCenterCard(clickedCard);
+                Game.carteButtate.add(clickedCard);
+                Game.myCards.remove(clickedCard);
+                GUI.CardUpdateGUI();
+            }
+            System.out.println("carta cliccata"+clickedCard);
         }
     }
 
 }
-
