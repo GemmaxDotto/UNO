ef50c2afa72b235518dbdd28c7162a2b745a5a7e
GemmaxDotto <g.dotto05@gmail.com>
2023-11-22 20:10:16 +0100

    rimozione unoDeck ormai non utile, modifica Panel per far si che venga visualizzato correttamente

--- uno_nobuild/src/client/CardPanel.java
+++ uno_nobuild/src/client/CardPanel.java
@@ -1,38 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class CardPanel extends JPanel {
-    private UnoDeck unoDeck;
 
-    public CardPanel(UnoDeck unoDeck) {
-        this.unoDeck = unoDeck;
+    private GameManaging game;
+    ClientGUI GUI;
+
+
+    public CardPanel(GameManaging game,ClientGUI GUI) {
+        
+        this.GUI=GUI;
+        this.game=game;
 
         // Imposta il layout e aggiungi il listener del mouse
         setFocusable(true);  // Assicurati che il pannello sia cliccabile
         setLayout(new FlowLayout());
        // addMouseListener(new CardClickListener());
-        updateCards(); // Aggiorna le carte quando viene creato il pannello
+       // updateCards(); // Aggiorna le carte quando viene creato il pannello
     }
 
     // Metodo per aggiornare la rappresentazione grafica delle carte
     // Nel metodo updateCards di CardPanel
     public void updateCards() {
         removeAll(); // Rimuovi tutte le carte attuali
-
+    
         // Aggiungi le nuove carte
-        for (UnoCard card : unoDeck.getCards()) {
-            ImageIcon icon = new ImageIcon(card.getImagePath());
-            JLabel label = new JLabel(icon);
-            add(label);
+        for (UnoCard card : game.myCards) {
+            //ImageIcon icon = new ImageIcon(card.getImagePath());
+            CardComponent cardComp=new CardComponent(card, game,GUI);
+            
+        
+    
+            add(cardComp);
         }
-
+    
         revalidate(); // Aggiorna il layout
         repaint(); // Ridisegna il pannello
     }
-
+    
     // Metodo per aggiungere nuove carte nella parte superiore
     public void addCardsToTop(ArrayList<CardComponent> newCards) {
         JPanel newCardsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -59,21 +65,4 @@ public class CardPanel extends JPanel {
         repaint(); // Ridisegna il pannello
     }
 
-    /*
-     // Listener per il clic delle carte
-    private class CardClickListener extends MouseAdapter {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            // Gestisci il clic della carta
-            Component clickedComponent = getComponentAt(e.getPoint());
-            if (clickedComponent instanceof CardComponent) {
-                UnoCard clickedCard = ((CardComponent) clickedComponent).getCard();
-                GameManaging.tempCard = clickedCard;
-                GameManaging.receivedCard();
-                // Fai qualcosa con la carta cliccata
-                System.out.println("Hai cliccato su: " + clickedCard);
-
-            }
-        }
-    } */
 }
