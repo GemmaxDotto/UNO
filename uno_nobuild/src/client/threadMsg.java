2047c4598835189a00682b895213303ed1e0e0a2
manuel <manuel14.provenzi@gmail.com>
2023-11-24 22:58:30 +0100

    inizio thread

--- uno_nobuild/src/client/threadMsg.java
+++ uno_nobuild/src/client/threadMsg.java
@@ -4,6 +4,26 @@
  * 
  */
 public class threadMsg extends Thread{
-    
-    
+    Condivisa cond ;
+    public threadMsg(Condivisa cond){
+        this.cond = cond;
+    }
+
+    @Override
+    public void run() {
+       //ricezione msg per tutti i client
+       while (true) {
+            String receString=cond.Game.client.receiveMessage().strip();
+
+            switch (receString.split(";")[0]) {
+                //game,first,speciali 
+                //msg = "speciale;"+nickGiocatoreSuccessivo+";salta_turno"
+            
+                case "speciale":
+                    
+                    break;
+            }
+       }
+                
+    }
 }
