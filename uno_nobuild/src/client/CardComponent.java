7673b2b484ed03ba8fa99fe2e72467ab5490b0ae
GemmaxDotto <g.dotto05@gmail.com>
2023-11-24 23:05:12 +0100

    gestione messaggio affidato al GameManaging

--- uno_nobuild/src/client/CardComponent.java
+++ uno_nobuild/src/client/CardComponent.java
@@ -14,72 +14,36 @@ public class CardComponent extends JButton {
     GameManaging Game;
     ClientGUI GUI;
     JPanel parentPanel;
-    
 
     public CardComponent(UnoCard card, GameManaging Game, ClientGUI GUI) {
         this.card = card;
-        // setText(card.toString()); // Usa toString() o un'altra rappresentazione
-        // testuale della carta
-        // setOpaque(true);
-        // setBackground(Color.WHITE);
-        this.Game=Game;
-        this.GUI=GUI;
-       
-        ImageIcon originalIcon = new ImageIcon(card.getImagePath());
+        this.Game = Game;
+        this.GUI = GUI;
 
-        // Ridimensiona l'immagine
+        ImageIcon originalIcon = new ImageIcon(card.getImagePath());
         Image originalImage = originalIcon.getImage();
-        Image scaledImage = originalImage.getScaledInstance(50, 80, Image.SCALE_SMOOTH); // Imposta le dimensioni
-                                                                                         // desiderate
+        Image scaledImage = originalImage.getScaledInstance(50, 80, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
         setIcon(scaledIcon);
         setPreferredSize(new Dimension(50, 80)); // Imposta le dimensioni desiderate
         setFocusable(true); // Abilita l'evento di input
         addMouseListener(new CardClickListener());
-        // setBorder(BorderFactory.createLineBorder(Color.RED)); // Cambia il colore a
-        // tuo piacimento
-        // Oppure
-        // setBackground(Color.YELLOW); // Cambia il colore a tuo piacimento */
+
     }
 
     public UnoCard getCard() {
         return card;
     }
 
-   
     private class CardClickListener extends MouseAdapter {
-        
-    
+
         @Override
         public void mouseClicked(java.awt.event.MouseEvent e) {
             UnoCard clickedCard = getCard();
 
-            // Invia un messaggio al server
-            // Assicurati che il tuo oggetto GameManaging abbia un metodo
-            // sendMessageToServer()
-            // che possa inviare il messaggio al server
-            //&&clickedCard.toString().(Game.myCards)
-            if (Game != null&&!clickedCard.getColore().equals("K")&&!clickedCard.toString().equals(GameManaging.tempCard.toString())) {
-                Game.client.sendMessage(Game.nickNameString+";"+"lascia;"+clickedCard.toString());
-                String receString=Game.client.receiveMessage().strip().split(";")[3];
-                if(receString.equals("ok")){
-                    //gestire ricezione mess speciali
-                Game.setCenterCard(clickedCard);
-                Game.carteButtate.add(clickedCard);
-                Game.myCards.remove(clickedCard);
-                GameManaging.tempCard=clickedCard;
-    
+            Game.handleLascia(clickedCard);
 
-                GUI.updatePlayerCards();
-                GUI.updateCentralCard();
-                }
-                
-                
-
-
-                
-            }
-            System.out.println("carta cliccata"+clickedCard);
+            System.out.println("carta cliccata" + clickedCard);
         }
     }
 
