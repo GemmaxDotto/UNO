5b29d24f1cb86b2741ce2a8e26309a7ee28b5b0a
GemmaxDotto <g.dotto05@gmail.com>
2023-11-24 23:07:09 +0100

    thread che e' sempre in ascolto e legato al GameManaging, Mancano gli altri messaggi che potrennero servire

--- uno_nobuild/src/client/threadMsg.java
+++ uno_nobuild/src/client/threadMsg.java
@@ -3,7 +3,37 @@
  *  thread per la ricezione dei messaggi client
  * 
  */
-public class threadMsg extends Thread{
-    
-    
+
+public class threadMsg extends Thread {
+
+    private TCPClient tcpClient;
+    private GameManaging Game;
+
+    public threadMsg(TCPClient tcpClient, GameManaging Game) {
+        this.tcpClient = tcpClient;
+        this.Game = Game;
+    }
+
+    public void start() {
+
+        // Avvio di un thread separato per la gestione dei messaggi dal server
+        new Thread(() -> {
+            try {
+
+                while (true) {
+                    String receivedMessage = tcpClient.receiveMessage();
+
+                    if (receivedMessage != null && receivedMessage.equals("GO")) {
+                        Game.on = true;
+                    }
+
+                    Thread.sleep(500); 
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
 }
