ba1789a3c6aa79d1ec8966b6833651a2a6dad8b5
GemmaxDotto <g.dotto05@gmail.com>
2023-11-27 18:38:30 +0100

    Fix threadMsg class logic, handle received messages properly and new messages from server

--- uno_nobuild/src/client/threadMsg.java
+++ uno_nobuild/src/client/threadMsg.java
@@ -28,11 +28,33 @@ public class threadMsg extends Thread {
                     if (receivedMessage != null && receivedMessage.equals("GO")) {
                         cond.Game.on = true;
                     }
-                    if(receivedMessage.strip().equals("ok")){
+                    if(receivedMessage != null && receivedMessage.strip().equals("ok")){
                         cond.Game.gestisciRispostaLascia();
                         System.out.println("ok ricevuto");
                     }
-    
+                    if(receivedMessage != null &&receivedMessage.strip().split(";")[0].equals("CentralCard")){
+                        cond.tempCard = GameManaging.fromString(receivedMessage.strip().split(";")[1]);
+                        cond.Game.numeroAvv=Integer.parseInt(receivedMessage.strip().split(";")[2]);
+                        System.out.println("carta centrale ricevuta");
+                    }
+                    if(receivedMessage != null &&receivedMessage.strip().split(";")[0].equals("pesca")){
+                        cond.Game.gestisciRispostaPesca(receivedMessage.strip().split(";")[2]);
+                        System.out.println("carta pescata");
+                    }
+                    /* if(receivedMessage != null &&receivedMessage.strip().split(";")[0].equals("uno")){
+                        cond.Game.gestisciRispostaUno(receivedMessage.strip().split(";")[1]);
+                        System.out.println("uno ricevuto");
+                    }
+                    if(receivedMessage != null &&receivedMessage.strip().split(";")[0].equals("vittoria")){
+                        cond.Game.gestisciRispostaVittoria(receivedMessage.strip().split(";")[1]);
+                        System.out.println("vittoria ricevuta");
+                    }*/
+                    //risposta con carta centrale da cambiare
+                    if(receivedMessage != null &&receivedMessage.strip().split(";")[0].equals("CentralChangeCard")){
+                        cond.Game.gestisciRispostaCambia(receivedMessage.strip().split(";")[1]);
+                        System.out.println("cambia centrale");
+                    }
+     
 
                     Thread.sleep(500); 
                 }
